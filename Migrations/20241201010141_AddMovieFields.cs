697a66e3c885d220b4c71594d1b7c1882bb67fa7
Kabita Shrestha <kabitashrestha0009@gmail.com>
2024-11-30 21:10:55 -0500

    Movie trivia and daily quote

---
+++ Migrations/20241201010141_AddMovieFields.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Director",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1,
+                columns: new[] { "Director", "Duration", "ReleaseDate" },
+                values: new object[] { "Jackie Chan", 120, new DateTime(1944, 12, 23, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2,
+                columns: new[] { "Director", "Duration", "ReleaseDate" },
+                values: new object[] { "Haiyaaaa", 120, new DateTime(1944, 12, 23, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3,
+                columns: new[] { "Director", "Duration", "ReleaseDate" },
+                values: new object[] { "Buddha Mula", 120, new DateTime(1944, 12, 23, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Director",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ Migrations/20241201015954_MovieQuote.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieList.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovieQuote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Trivia",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1,
+                column: "Trivia",
+                value: "Something weired happens!");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2,
+                column: "Trivia",
+                value: "Wow, what a wonder woman");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3,
+                columns: new[] { "Director", "Trivia" },
+                values: new object[] { "Mula", "Jackie Chan rocks" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Trivia",
+                table: "Movies");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3,
+                column: "Director",
+                value: "Buddha Mula");
+        }
+    }
+}
